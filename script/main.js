acbfa3ddfc6dde1ba5f03cfea56eb1148496ae93
ysy95 <ysy95@naver.com>
2023-01-15 18:00:00 +0900

    main

--- script/main.js
+++ script/main.js
@@ -1,15 +1,17 @@
-let ch_gallery = document.getElementsByClassName('.ch_gallery');
+// let ch_gallery = document.querySelector('.channel > .ch_wrap > ul');
 
-window.addEventListener('scroll', ()=>{
-  let scrollUp = window.scrollY;
-  console.log(scrollUp);
-
-  if(scrollUp > 1700){
-    ch_gallery.style.anamation = 'fadeInUp is ease-out';
-  }
-});
-
-// $(()=>{
+// // 스크롤이벤트
+// window.addEventListener('scroll', ()=>{
+//   let scrollUp = window.scrollY; //스크롤의 높이를 가져온다
+//   // console.log(scrollUp);
+//   if(scrollUp > 1000){
+//     ch_gallery.style.animate ='slideUp is ease-in';
+//   }
+// });
 
 
+// 탑버튼
+// $('.top_btn').click(function(){
+//   $('html,body').animate({scrollTop:'0px'},300);
+//   return false;
 // });
