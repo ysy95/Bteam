303375e25f5801bd1ab62f1f342f23d19dbb58cd
ysy95 <ysy95@naver.com>
2023-01-20 03:23:20 +0900

    pc버전 내비수정 및 취합

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
 let gallery = document.querySelector('.ch_gallery');
 
 window.addEventListener('scroll',()=>{
-  let scrollUp = window.scrollY;
+  let scrollUp1 = window.scrollY;
   // console.log('scrollY',scrollUp);
 
-  if(scrollUp > 1400){
+  if(scrollUp1 > 1400){
     gallery.style.animation = 'fadeIn 2s ease-out ';
   }
   else{
@@ -14,8 +14,11 @@ window.addEventListener('scroll',()=>{
 
 
 
+
 // 제이쿼리
 $(()=>{
+
+  
   let top_btn = $('#top_btn');
   
   //  top버튼
@@ -34,97 +37,63 @@ $(()=>{
   
   
   // pc버전 디즈니 필름 부분 사진에 마우스 오버시 글씨 나오게하기
-  let film_img1 = $('.disney_films > ul > li:first-of-type');
-  let film_txt1 = $('.disney_films > ul > li:first-of-type > .film_txt');
-
-  film_img1.mouseenter(()=>{
-      film_txt1.show().css({
-        'scale':'1.02',
-        'filter':'contrast(30%)',
-        'transition':'ease-out'
-      });
+  // 마우스 오버시 글시 애니메이션
+  $('#film_img1').mouseenter(()=>{
+    $('#film_txt01').animate({'left':'25%'},800).css({
+      'opacity':'1',
+      'transition':'0.5s'
+    });
+  });
+  $('#film_img2').mouseenter(()=>{
+    $('#film_txt02').animate({'left':'25%'},1000).css({
+      'opacity':'1',
+      'transition':'1s ease-out'
+    });
   });
 
+  $('#film_img3').mouseenter(()=>{
+    $('#film_txt03').animate({'left':'25%'},800).css({
+      'opacity':'1',
+      'transition':'0.5s ease-out'
+    });
 
-  //모바일버전 메뉴보기 버튼
-  $('.total_btn').click(()=>{
-    $('.h_bottom').fadeIn();
-    $('.gnb').fadeIn();
-    $('.lnb').fadeIn();
   });
-  // 모바일버전 내비게이션 닫기버튼
-  $('.close_btn').click(()=>{
-    $('.gnb').hide(500);
-    $('.lnb').hide(180);
-    $('.h_bottom').hide();
+  // 마우스 뗴면 돌아가는 애니메이션
+  $('#film_img1').mouseleave(()=>{
+    $('#film_txt01').animate({'left':'-400px'},800).css({
+      'opacity':'0',
+      'transition':'1-0.5s ease-out',
+    });
+    $('.disney_films ul > #film_img1').css({
+      'filter':'contrast(100%)',
+      'transform':'scale(1)'
+    });
   });
 
-  // 서브메뉴 나오게하기
-  let sub_btn1 = $('li:first-of-type .sub_btn');
-  let sub1 = $('li:first-of-type .sub');
-  let sub_btn2 = $('li:nth-of-type(2) .sub_btn');
-  let sub2 = $('li:nth-of-type(2) .sub');
-  let sub_btn3 = $('li:nth-of-type(3) .sub_btn');
-  let sub3 = $('li:nth-of-type(3) .sub');
-  let sub_btn4 = $('li:nth-of-type(4) .sub_btn');
-  let sub4 = $('li:nth-of-type(4) .sub_btn');
-  let sub_btn5 = $('li:last-of-type .sub_btn');
-  let sub5 = $('li:last-of-type .sub');
-  let n = 1;
+  $('#film_img2').mouseleave(()=>{
+    $('#film_txt02').animate({'left':'-400px'},1000).css({
+      'opacity':'0',
+      'transition':'1s ease-out',
+    });
+    $('.disney_films ul > #film_img2').css({
+      'filter':'contrast(100%)',
+      'transform':'scale(1)'
+    });
+  });
+  
+  $('#film_img3').mouseleave(()=>{
+    $('#film_txt03').animate({'left':'-400px'},1000).css({
+      'opacity':'0',
+      'transition':'1s ease-out',
+    });
+    $('.disney_films ul > #film_img3').css({
+      'filter':'contrast(100%)',
+      'transform':'scale(1)'
+    });
+  });
 
-  sub_btn1.click(()=>{
-    if(n == 1){
-      sub1.show(150);
-      sub_btn1.css('transform','rotate(180deg)');
-      n = 0;
-    }else{
-      sub1.hide(200);
-      sub_btn1.css('transform','rotate(0deg)');
-      n = 1;
-    }
-  });
-  sub_btn2.click(()=>{
-    if(n == 1){
-      sub2.show(150);
-      sub_btn2.css('transform','rotate(180deg)');
-      n = 0;
-    }else{
-      sub2.hide(200);
-      sub_btn2.css('transform','rotate(0deg)');
-      n = 1;
-    }
-  });
- sub_btn3.click(()=>{
-  if(n == 1){
-    sub3.show(150);
-    sub_btn3.css('transform','rotate(180deg)');
-    n = 0;
-  }else{
-    sub3.hide(200);
-    sub_btn3.css('transform','rotate(0deg)');
-    n = 1;
-  }
-  });
-  sub_btn4.click(()=>{
-    if(n == 1){
-      sub4.show(150);
-      sub_btn4.css('transform','rotate(180deg)');
-      n = 0;
-    }else{
-      sub4.hide(200);
-      sub_btn4.css('transform','rotate(0deg)');
-      n = 1;
-    }
-  });
-  sub_btn5.click(()=>{
-    if(n == 1){
-      sub5.show(150);
-      sub_btn5.css('transform','rotate(180deg)');
-      n = 0;
-    }else{
-      sub5.hide(200);
-      sub_btn5.css('transform','rotate(0deg)');
-      n = 1;
-    }
-  });
+
+  // 태블릿
+  
+
 });
